3d2342860ac7508f8295fd95c701559149d58549
Ivor Wanders <ivor@iwanders.net>
2024-12-24 13:55:29 -0500

    Add documentation for Transformed trait.

--- src/transform.rs
+++ src/transform.rs
@@ -1,19 +1,24 @@
 
 pub trait Transformed : svg::Node{
+    /// Translate this element in x direction by the provided value.
     fn translated_x<T: Into<svg::node::Value>>(self, x: T) -> Self where Self: Sized {
         let mut z = self;
         z.assign("transform", format!("translate({},0.0)", x.into()));
         z
     }
+    /// Translate this element in y direction by the provided value.
     fn translated_y<T: Into<svg::node::Value>>(self, y: T) -> Self where Self: Sized {
         let mut z = self;
         z.assign("transform", format!("translate(0.0,{})", y.into()));
         z
     }
+    /// Translate this element in x and y direction by the provided value.
     fn translated_xy<T: Into<svg::node::Value>, U: Into<svg::node::Value>>(self, x: T, y: U) -> Self where Self: Sized {
         let mut z = self;
         z.assign("transform", format!("translate({},{})", x.into(), y.into()));
         z
     }
 }
+
+// Blanket implementation for all nodes, since transform is applicable to anything.
 impl<T: svg::node::Node> Transformed for T {}
